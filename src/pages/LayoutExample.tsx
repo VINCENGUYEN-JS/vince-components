2c8b3942be26ec1ecbd54bb824ce8d459705b82b
VinceNguyen <nguyenquangv95@gmail.com>
2022-02-02 15:16:25 +0700

    add-more-examples-to-grid-layout

--- src/pages/LayoutExample.tsx
+++ src/pages/LayoutExample.tsx
@@ -1,12 +1,41 @@
 import * as React from "react";
 import { Row, Col } from "../layout";
 
+const BorderBox = (props: any) => (
+  <div
+    style={{
+      border: "1px solid #ccc",
+      backgroundColor: "#92a8d1",
+      padding: "10px",
+    }}
+  >
+    {props.children}
+  </div>
+);
+
 const LayoutExample = () => (
-  <Row>
-    <Col span={8}>col1</Col>
-    <Col span={8}>col1</Col>
-    <Col span={8}>col1</Col>
-  </Row>
+  <>
+    <Row>
+      <Col span={8}>
+        <BorderBox>33%</BorderBox>
+      </Col>
+      <Col span={8}>
+        <BorderBox>33%</BorderBox>
+      </Col>
+      <Col span={8}>
+        <BorderBox>33%</BorderBox>
+      </Col>
+    </Row>
+    <br />
+    <Row>
+      <Col span={12}>
+        <BorderBox>50%</BorderBox>
+      </Col>
+      <Col span={12}>
+        <BorderBox>50%</BorderBox>
+      </Col>
+    </Row>
+  </>
 );
 
 export default LayoutExample;
