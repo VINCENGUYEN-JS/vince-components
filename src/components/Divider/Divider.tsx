8ac3e1981a4f26bee029839aed0787aa750272eb
VinceNguyen <nguyenquangv95@gmail.com>
2022-02-04 19:47:01 +0700

    add-support-for-left-text-divider

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -8,14 +8,16 @@ const prefixCls = "divider";
 type DividerProps = {
   children?: React.ReactNode;
   type?: "horizontal" | "vertical";
+  orientation?: "left" | "right" | "center";
 };
 
 const Divider = (props: DividerProps) => {
-  const { children, type = "horizontal" } = props;
+  const { children, type = "horizontal", orientation = "center" } = props;
   const hasChildren = !!children;
   const classes = classNames(prefixCls, {
     [`${prefixCls}-${type}`]: type,
     [`${prefixCls}-with-text`]: hasChildren,
+    [`${prefixCls}-with-text-${orientation}`]: hasChildren,
   });
   return <div className={classes}>{children}</div>;
 };
--- src/pages/ButtonExample.tsx
+++ src/pages/ButtonExample.tsx
@@ -2,34 +2,33 @@ import * as React from "react";
 
 import { DownloadOutlined } from "@ant-design/icons";
 import Button from "../components/Button/Button";
+import Divider from "../components/Divider/Divider";
 
 const ButtonExample = () => (
   <div style={{ marginTop: "20px" }}>
+    <Divider orientation="left">Button With Primary + Large</Divider>
     <Button type="primary" size="large">
       Primary
     </Button>
-    <br />
-    <br />
+    <Divider orientation="left">Button With Primary + Small</Divider>
     <Button type="dashed" size="small">
       Primary
     </Button>
-    <br />
-    <br />
+    <Divider orientation="left">Button With Primary + Round</Divider>
     <Button type="primary" shape="round">
       I'm Round
     </Button>
-    <br />
-    <br />
+    <Divider orientation="left">Button With Primary + Round + Large</Divider>
     <Button type="primary" shape="round" size="large">
       I'm Round And Large
     </Button>
-    <br />
-    <br />
+    <Divider orientation="left">Button With Primary + Circle + Large</Divider>
     <Button type="primary" shape="circle" size="large">
       I'm Circle And Large
     </Button>
-    <br />
-    <br />
+    <Divider orientation="left">
+      Button With Primary + Round + Large + Icon
+    </Divider>
     <Button
       type="primary"
       icon={<DownloadOutlined />}
