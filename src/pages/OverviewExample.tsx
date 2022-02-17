e4db0072512403add4392bd3466c0aa9e1b3b1bb
VinceNguyen <nguyenquangv95@gmail.com>
2022-02-17 20:26:08 +0700

    add-more-contents-to-overview-page

--- src/pages/OverviewExample.tsx
+++ src/pages/OverviewExample.tsx
@@ -1,18 +1,44 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 import ComponentDoc from "../template/ComponentDoc";
 import Divider from "../components/Divider/Divider";
 import Input from "../components/Input";
+import Card from "../components/Card";
+import { Row, Col } from "../layout";
+
+const styleImg = {
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "152px",
+};
 
 const Overview = () => (
   <>
     <ComponentDoc
       title="Component Overview"
       introduction="Provide you plenty of UI components to enrich your web application"
-    />
-    <Divider />
-    <Input.Search placeholder="Search in components" />
-    <Divider />
+    >
+      <Divider />
+      <Input.Search placeholder="Search in components" />
+      <Divider />
+      <h2 className="component-overview-group-title">General</h2>
+      <Row>
+        <Col span={6}>
+          <Link to="/button">
+            <Card title="Button">
+              <div style={styleImg}>
+                <img
+                  src="https://gw.alipayobjects.com/zos/alicdn/fNUKzY1sk/Button.svg"
+                  alt="Button"
+                />
+              </div>
+            </Card>
+          </Link>
+        </Col>
+      </Row>
+    </ComponentDoc>
   </>
 );
 
--- src/template/ComponentDoc.tsx
+++ src/template/ComponentDoc.tsx
@@ -5,15 +5,17 @@ import "./style/index.scss";
 type ComponentDocProps = {
   title: string;
   introduction: string;
+  children?: React.ReactNode;
 };
 
 const ComponentDoc = (props: ComponentDocProps) => {
-  const { title, introduction } = props;
+  const { title, introduction, children } = props;
   return (
     <section className="markdown">
       <h1>{title}</h1>
       <p>{introduction}</p>
       <h2>Examples</h2>
+      {children}
     </section>
   );
 };
