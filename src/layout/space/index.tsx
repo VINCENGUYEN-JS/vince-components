5e601f8e4cb551c8e4c6ed8b472d70072eb08ad7
VinceNguyen <nguyenquangv95@gmail.com>
2022-02-26 10:21:31 +0700

    add-support-for-vertial-mode-and-gap

--- src/layout/space/index.tsx
+++ src/layout/space/index.tsx
@@ -7,23 +7,36 @@ import "./style/index.scss";
 
 const prefixCls = "space";
 
-interface TagProps extends React.HTMLAttributes<HTMLDivElement> {
+interface SpaceProps extends React.HTMLAttributes<HTMLDivElement> {
   align?: "start" | "end" | "center" | "baseline";
   direction?: "horizontal" | "vertical";
+  size?: "small" | "middle" | "large";
 }
 
-const Space = (props: TagProps) => {
+const spaceSize = {
+  small: 8,
+  middle: 16,
+  large: 24,
+};
+
+function getNumberSize(size: keyof typeof spaceSize) {
+  return typeof size === "string" ? spaceSize[size] : size || 0;
+}
+
+const Space = (props: SpaceProps) => {
   const {
     children,
     className,
     align,
     direction = "horizontal",
     style: customStyle,
+    size = "small",
   } = props;
   const mergedAlign =
     align === undefined && direction === "horizontal" ? "center" : align;
   const classes = classNames(
     prefixCls,
+    `${prefixCls}-${direction}`,
     {
       [`${prefixCls}-align-${mergedAlign}`]: mergedAlign,
     },
@@ -31,8 +44,12 @@ const Space = (props: TagProps) => {
   );
   const childNodes = toArray(children, { keepEmpty: true });
   const nodes = childNodes.map((child) => <Item key={child.key}>{child}</Item>);
+  const style = {
+    ...customStyle,
+    gap: getNumberSize(size),
+  };
   return (
-    <div className={classes} style={customStyle}>
+    <div className={classes} style={style}>
       {nodes}
     </div>
   );
--- src/pages/layouts/SpaceExample.tsx
+++ src/pages/layouts/SpaceExample.tsx
@@ -3,11 +3,18 @@ import * as React from "react";
 import { Space } from "layout";
 import ComponentDoc from "template/ComponentDoc";
 import Button from "components/Button/Button";
+import Divider from "components/Divider/Divider";
 
 const SpaceExample = () => {
   return (
     <ComponentDoc title="Space" introduction="Set components spacing">
+      <Divider> [horizontal]</Divider>
       <Space>
+        space
+        <Button type="primary">Button</Button>
+      </Space>
+      <Divider>[vertical]</Divider>
+      <Space direction="vertical">
         space
         <Button type="primary">Button</Button>
       </Space>
