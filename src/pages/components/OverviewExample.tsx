372c691eea256e9d2dccc5b11ea979f74d3a841a
VinceNguyen <nguyenquangv95@gmail.com>
2022-02-26 15:53:36 +0700

    add-more-components-to-overview-page

--- src/pages/components/OverviewExample.tsx
+++ src/pages/components/OverviewExample.tsx
@@ -8,6 +8,17 @@ import Card from "components/Card";
 import Tag from "components/Tag";
 import { Row, Col, Space } from "layout";
 
+type TextWithSpaceProps = {
+  title: string;
+  quantity: number;
+};
+
+type CardLinkComponentProps = {
+  src: string;
+  to: string;
+  title: string;
+};
+
 const styleImg = {
   display: "flex",
   alignItems: "center",
@@ -15,37 +26,88 @@ const styleImg = {
   height: "152px",
 };
 
-const Overview = () => (
-  <>
-    <ComponentDoc
-      title="Component Overview"
-      introduction="Provide you plenty of UI components to enrich your web application"
-    >
-      <Divider />
-      <Input.Search placeholder="Search in components" />
-      <Divider />
-      <h2 className="component-overview-group-title">
-        <Space style={{ gap: "8px" }}>
-          General
-          <Tag>1</Tag>
-        </Space>
-      </h2>
-      <Row>
-        <Col span={6}>
-          <Link to="/button">
-            <Card title="Button">
-              <div style={styleImg}>
-                <img
-                  src="https://gw.alipayobjects.com/zos/alicdn/fNUKzY1sk/Button.svg"
-                  alt="Button"
-                />
-              </div>
-            </Card>
-          </Link>
-        </Col>
-      </Row>
-    </ComponentDoc>
-  </>
+const TextWithSpace = ({ title, quantity }: TextWithSpaceProps) => (
+  <h2 className="component-overview-group-title">
+    <Space>
+      {title}
+      <Tag>{quantity}</Tag>
+    </Space>
+  </h2>
 );
+
+const CardLinkComponent = ({ src, to, title }: CardLinkComponentProps) => (
+  <Link to={to}>
+    <Card title={title}>
+      <div style={styleImg}>
+        <img src={src} alt={title} />
+      </div>
+    </Card>
+  </Link>
+);
+
+const overviewComponents = {
+  general: [
+    {
+      to: "/button",
+      title: "Button",
+      src: "https://gw.alipayobjects.com/zos/alicdn/fNUKzY1sk/Button.svg",
+    },
+  ],
+  layouts: [
+    {
+      to: "/divider",
+      title: "Divider",
+      src: "https://gw.alipayobjects.com/zos/alicdn/5swjECahe/Divider.svg",
+    },
+    {
+      to: "/grid",
+      title: "Grid",
+      src: "https://gw.alipayobjects.com/zos/alicdn/5rWLU27so/Grid.svg",
+    },
+    {
+      to: "/layout",
+      title: "Layout",
+      src: "https://gw.alipayobjects.com/zos/alicdn/5swjECahe/Divider.svg",
+    },
+    {
+      to: "/space",
+      title: "Space",
+      src: "https://gw.alipayobjects.com/zos/antfincdn/wc6%263gJ0Y8/Space.svg",
+    },
+  ],
+};
+
+const Overview = () => {
+  const generalComponents = overviewComponents.general;
+  const layoutComponents = overviewComponents.layouts;
+  return (
+    <>
+      <ComponentDoc
+        title="Component Overview"
+        introduction="Provide you plenty of UI components to enrich your web application"
+      >
+        <Divider />
+        <Input.Search placeholder="Search in components" />
+        <Divider />
+        <TextWithSpace title="General" quantity={1} />
+        <Row gutter={12}>
+          {generalComponents.map((general) => (
+            <Col span={6}>
+              <CardLinkComponent {...general} />
+            </Col>
+          ))}
+        </Row>
+        <TextWithSpace title="Layout" quantity={4} />
+        <Row gutter={12}>
+          {layoutComponents.map((layout) => (
+            <Col span={6}>
+              <CardLinkComponent {...layout} />
+            </Col>
+          ))}
+        </Row>
+      </ComponentDoc>
+    </>
+  );
+};
 
 export default Overview;
