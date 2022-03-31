83effae27a9f52c764f18f8d8b8cbb2d937e7889
VinceNguyen <nguyenquangv95@gmail.com>
2022-03-31 15:34:50 +0700

    add-support-for-more-title-component

--- src/components/src/Typography/Title.tsx
+++ src/components/src/Typography/Title.tsx
@@ -1,13 +1,21 @@
 import * as React from 'react';
 import Base from './Base';
 
+const TITLE_ELE_LIST = [1, 2, 3, 4, 5];
+
 type TitleProps = {
   children: React.ReactNode;
+  level?: typeof TITLE_ELE_LIST[number];
 };
 
 const Title: React.FC<TitleProps> = (props) => {
-  const { children } = props;
-  const component = 'h1';
+  const { level = 1, children } = props;
+  let component: string;
+  if (TITLE_ELE_LIST.includes(level)) {
+    component = `h${level}`;
+  } else {
+    component = 'h1';
+  }
 
   return <Base component={component}> {children} </Base>;
 };
--- src/pages/components/TypographyExample.tsx
+++ src/pages/components/TypographyExample.tsx
@@ -3,14 +3,25 @@ import * as React from 'react';
 import ComponentDoc from '../../template/ComponentDoc';
 import { Divider, Typography } from '@vince-components';
 
+const apiCode = `<Typography>
+  <Typography.Title>h1.Vince Design</Typography.Title>
+  <Typography.Title level={2}>h2.Vince Design</Typography.Title>
+  <Typography.Title level={3}>h3.Vince Design</Typography.Title>
+  <Typography.Title level={4}>h4.Vince Design</Typography.Title>
+</Typography>`;
+
 const TypographyExample = () => (
   <>
     <ComponentDoc
       title="Typography"
       introduction="Basic text writing, including headings, body text, lists, and more."
+      code={apiCode}
     >
       <Typography>
-        <Typography.Title>Introduction</Typography.Title>
+        <Typography.Title>h1.Vince Design</Typography.Title>
+        <Typography.Title level={2}>h2.Vince Design</Typography.Title>
+        <Typography.Title level={3}>h3.Vince Design</Typography.Title>
+        <Typography.Title level={4}>h4.Vince Design</Typography.Title>
       </Typography>
     </ComponentDoc>
   </>
