40daf92538961799e311680bc098ac4f7bba8970
VinceNguyen <nguyenquangv95@gmail.com>
2022-04-01 09:34:00 +0700

    Updates

--- src/components/src/Input/index.tsx
+++
@@ -1,9 +0,0 @@
-import InternalInput, { InputProps } from './Input';
-import Search from './Search';
-
-type InputComposition = React.FC<InputProps> & { Search: typeof Search };
-
-const Input = InternalInput as InputComposition;
-Input.Search = Search;
-
-export default Input;
